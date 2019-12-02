a6077331a1d6d081e5e995241ff3f02765a1ce50
Oskar Ek <oskarek24@gmail.com>
2019-12-02 22:58:17 +0100

    Added solution for day 1 problems

--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -1,10 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Day1.Day1 ( problem ) where
 
-import Problem
-import qualified Data.Text as T
+import           Problem
+import qualified Data.Text                     as T
+import qualified Parsing                       as P
+import           Control.Arrow                  ( (&&&) )
+
+f :: Int -> Int
+f x = x `div` 3 - 2
+
+g :: Int -> Int
+g x = if y > 0 then y + g y else 0 where y = f x
 
 problem :: Problem
-problem = Problem
-  { parse = Right
-  , solve = return ("", "") }
+problem = Problem { parse = mapM (P.parse P.int)
+                  , solve = sumWith f &&& sumWith g
+                  }
+  where sumWith f = sum . map f
