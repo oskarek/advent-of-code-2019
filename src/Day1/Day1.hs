b67ca5d5c96f5503bc084d108c07a2983b7d5f2a
Oskar Ek <oskarek24@gmail.com>
2019-12-03 10:07:35 +0100

    Don't split input into lines for the parse function

--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -3,6 +3,7 @@ module Day1.Day1 ( problem ) where
 import           Problem
 import qualified Data.Text                     as T
 import qualified Parsing                       as P
+import           Text.Parsec             hiding ( parse )
 import           Control.Arrow                  ( (&&&) )
 
 f :: Int -> Int
@@ -12,7 +13,7 @@ g :: Int -> Int
 g x = if y > 0 then y + g y else 0 where y = f x
 
 problem :: Problem
-problem = Problem { parse = mapM (P.parse P.int)
+problem = Problem { parse = P.parse $ many P.int
                   , solve = sumWith f &&& sumWith g
                   }
   where sumWith f = sum . map f
--- src/Problem.hs
+++ src/Problem.hs
@@ -6,10 +6,10 @@ import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 
 data Problem = forall a b. Show b =>
-  Problem { parse :: [Text] -> Either ParseError a
+  Problem { parse :: Text -> Either ParseError a
           , solve :: a -> (b, b) }
 
 solveProblem :: Problem -> Text -> Either ParseError (Text, Text)
 solveProblem Problem {..} input = do
-  (sol1, sol2) <- solve <$> parse (T.lines input)
+  (sol1, sol2) <- solve <$> parse input
   return (T.pack (show sol1), T.pack (show sol2))
