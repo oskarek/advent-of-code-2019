e617d3a21d4b87d5726011662fdc54088c4a696b
Oskar Ek <oskarek24@gmail.com>
2019-12-04 11:21:01 +0100

    Fix for indentation in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -10,24 +10,20 @@ import qualified Day1.Day1                     as Day1
 import qualified Day2.Day2                     as Day2
 
 problems :: Map Int Problem
-problems = M.fromList
-    [ (1, Day1.problem)
-    , (2, Day2.problem)
-    ]
+problems = M.fromList [(1, Day1.problem), (2, Day2.problem)]
 
 main :: IO ()
 main = do
-    day <- execParser opts
-    case problems !? day of
-        Nothing -> putStrLn $ "Day " ++ show day ++ " problem not solved yet!"
-        Just problem -> do
-            input <- TIO.readFile ("input/day" ++ show day ++ ".txt")
-            putStrLn $ printSolution problem input
-  where
-    opts = info
-        (day <**> helper)
-        (fullDesc <> progDesc "Solve the puzzles for DAY" <> header
-            "Advent of Code 2018 solutions"
-        )
-    day =
-        argument auto (help "The number of the day to solve" <> metavar "DAY")
+  day <- execParser opts
+  case problems !? day of
+    Nothing      -> putStrLn $ "Day " ++ show day ++ " problem not solved yet!"
+    Just problem -> do
+      input <- TIO.readFile ("input/day" ++ show day ++ ".txt")
+      putStrLn $ printSolution problem input
+ where
+  opts = info
+    (day <**> helper)
+    (fullDesc <> progDesc "Solve the puzzles for DAY" <> header
+      "Advent of Code 2018 solutions"
+    )
+  day = argument auto (help "The number of the day to solve" <> metavar "DAY")
